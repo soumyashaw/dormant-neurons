45471925ec844713d33209c1a3b52179b18a758f
Soumya Shaw <37841510+soumyashaw@users.noreply.github.com>
2025-03-15 19:55:38 +0100

    Update interface.py

--- user_interface/interface.py
+++ user_interface/interface.py
@@ -16,6 +16,15 @@ class MainWindow(QMainWindow):
         
         # Sidebar Navigation
         navBarLayout = QVBoxLayout()
+
+        self.buttonStyleDescriptionClicked = """QPushButton {
+                                                font-size: 25px;
+                                                background-color: rgba(255, 255, 255, 100);
+                                                padding: 20px;
+                                                border-radius: 10px;
+                                                font-weight: bold;
+                                            }
+                                        """
         
         self.buttonStyleDescriptionTransparent = """
             QPushButton {
@@ -53,7 +62,7 @@ class MainWindow(QMainWindow):
         navBar.setLayout(navBarLayout)
         navBar.setStyleSheet("background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #B721FF, stop: 1 #21D4FD);")
         
-        # Main Context: Paper Area ###################################################
+####### Main Context: Paper Area ###################################################
         # Tab Widget
         self.tabsPaper = QTabWidget()
         
@@ -279,8 +288,13 @@ class MainWindow(QMainWindow):
                                     }
                                 """)
         
+        self.mainContextLayout = QVBoxLayout()
+        self.mainContextLayout.addWidget(self.tabsPaper)
+        
 ############################### Main Context: Team Area ###################################################
-"""
+        # Tab Widget
+        self.tabsTeam = QTabWidget()
+        
         # Tab: Add Member
         tabAddMember = QWidget()
         layoutAddMember = QVBoxLayout()
@@ -363,123 +377,123 @@ class MainWindow(QMainWindow):
         
         formLayout5 = QGridLayout()
 
-        scroll2 = QScrollArea()
-        scroll2.setWidgetResizable(True)
-        scroll2.setStyleSheet("background-color: #d9d9d9; margin-bottom: 30px;")
+        scroll5 = QScrollArea()
+        scroll5.setWidgetResizable(True)
+        scroll5.setStyleSheet("background-color: #d9d9d9; margin-bottom: 30px;")
         
-        dateLabel2 = QLabel("Date")
-        dateLabel2.setStyleSheet(labelStyle)
+        dateLabel5 = QLabel("Date")
+        dateLabel5.setStyleSheet(labelStyle)
         
-        dateEdit2 = QDateEdit()
-        dateEdit2.setCalendarPopup(True)
-        dateEdit2.setStyleSheet(textBoxStyle)
-        dateEdit2.setDate(QDate.currentDate())
+        dateEdit5 = QDateEdit()
+        dateEdit5.setCalendarPopup(True)
+        dateEdit5.setStyleSheet(textBoxStyle)
+        dateEdit5.setDate(QDate.currentDate())
         
-        linkLabel2 = QLabel("Link")
-        linkLabel2.setStyleSheet(labelStyle)
-        linkTextBox2 = QLineEdit()
-        linkTextBox2.setStyleSheet(textBoxStyle)
+        linkLabel5 = QLabel("Link")
+        linkLabel5.setStyleSheet(labelStyle)
+        linkTextBox5 = QLineEdit()
+        linkTextBox5.setStyleSheet(textBoxStyle)
         
-        titleLabel2 = QLabel("Name")
-        titleLabel2.setStyleSheet(labelStyle)
-        titleTextBox2 = QLineEdit()
-        titleTextBox2.setStyleSheet(textBoxStyle)
+        titleLabel5 = QLabel("Name")
+        titleLabel5.setStyleSheet(labelStyle)
+        titleTextBox5 = QLineEdit()
+        titleTextBox5.setStyleSheet(textBoxStyle)
         
-        authorLabel2 = QLabel("Author")
-        authorLabel2.setStyleSheet(labelStyle)
-        authorTextBox2 = QLineEdit()
-        authorTextBox2.setStyleSheet(textBoxStyle)
+        authorLabel5 = QLabel("Author")
+        authorLabel5.setStyleSheet(labelStyle)
+        authorTextBox5 = QLineEdit()
+        authorTextBox5.setStyleSheet(textBoxStyle)
         
-        conferenceLabel2 = QLabel("Conference")
-        conferenceLabel2.setStyleSheet(labelStyle)
-        conferenceTextBox2 = QLineEdit()
-        conferenceTextBox2.setStyleSheet(textBoxStyle)
+        conferenceLabel5 = QLabel("Conference")
+        conferenceLabel5.setStyleSheet(labelStyle)
+        conferenceTextBox5 = QLineEdit()
+        conferenceTextBox5.setStyleSheet(textBoxStyle)
         
-        bibtexLabel2 = QLabel("BibTex")
-        bibtexLabel2.setStyleSheet(labelStyle)
+        bibtexLabel5 = QLabel("BibTex")
+        bibtexLabel5.setStyleSheet(labelStyle)
         
-        extractButton2 = QPushButton("Extract")
-        extractButton2.setFixedWidth(140)
-        extractButton2.clicked.connect(self.extractClicked)
-        extractButton2.setStyleSheet("font-size: 20px; padding: 10px; border-radius: 15px; background-color: #4972FD; color: white;")
+        extractButton5 = QPushButton("Extract")
+        extractButton5.setFixedWidth(140)
+        extractButton5.clicked.connect(self.extractClicked)
+        extractButton5.setStyleSheet("font-size: 20px; padding: 10px; border-radius: 15px; background-color: #4972FD; color: white;")
         
-        bibtexTextBox2 = QTextEdit()
-        bibtexTextBox2.setStyleSheet(textBoxStyle)
-        bibtexTextBox2.setPlaceholderText("Enter BibTex here")
+        bibtexTextBox5 = QTextEdit()
+        bibtexTextBox5.setStyleSheet(textBoxStyle)
+        bibtexTextBox5.setPlaceholderText("Enter BibTex here")
 
-        idLabel2 = QLabel("Paper ID")
-        idLabel2.setStyleSheet(labelStyle)
-        idTextBox2 = QLineEdit()
-        idTextBox2.setStyleSheet(textBoxStyle)
+        idLabel5 = QLabel("Paper ID")
+        idLabel5.setStyleSheet(labelStyle)
+        idTextBox5 = QLineEdit()
+        idTextBox5.setStyleSheet(textBoxStyle)
 
-        submitButton2 = QPushButton("Submit Paper")
-        submitButton2.setFixedWidth(300)
-        submitButton2.clicked.connect(self.submitPaperClicked)
-        submitButton2.setStyleSheet("font-size: 20px; padding: 10px; border-radius: 15px; background-color: #4972FD; color: white;")
+        submitButton5 = QPushButton("Submit Paper")
+        submitButton5.setFixedWidth(300)
+        submitButton5.clicked.connect(self.submitPaperClicked)
+        submitButton5.setStyleSheet("font-size: 20px; padding: 10px; border-radius: 15px; background-color: #4972FD; color: white;")
         
         # Adding widgets to Grid Layout
-        formLayout2.addWidget(scroll2, 0, 0, 1, 6)
+        formLayout5.addWidget(scroll5, 0, 0, 1, 6)
 
-        formLayout2.addWidget(dateLabel2, 1, 0, 1, 2)
-        formLayout2.addWidget(dateEdit2, 1, 1, 1, 1)
-        formLayout2.addWidget(linkLabel2, 1, 3, alignment=Qt.AlignmentFlag.AlignRight)
-        formLayout2.addWidget(linkTextBox2, 1, 4, 1, 2)
+        formLayout5.addWidget(dateLabel5, 1, 0, 1, 2)
+        formLayout5.addWidget(dateEdit5, 1, 1, 1, 1)
+        formLayout5.addWidget(linkLabel5, 1, 3, alignment=Qt.AlignmentFlag.AlignRight)
+        formLayout5.addWidget(linkTextBox5, 1, 4, 1, 2)
         
-        formLayout2.addWidget(titleLabel2, 2, 0)
-        formLayout2.addWidget(titleTextBox2, 2, 1, 1, 5)
+        formLayout5.addWidget(titleLabel5, 2, 0)
+        formLayout5.addWidget(titleTextBox5, 2, 1, 1, 5)
         
-        formLayout2.addWidget(authorLabel2, 3, 0)
-        formLayout2.addWidget(authorTextBox2, 3, 1, 1, 5)
+        formLayout5.addWidget(authorLabel5, 3, 0)
+        formLayout5.addWidget(authorTextBox5, 3, 1, 1, 5)
         
-        formLayout2.addWidget(conferenceLabel2, 4, 0)
-        formLayout2.addWidget(conferenceTextBox2, 4, 1, 1, 5)
+        formLayout5.addWidget(conferenceLabel5, 4, 0)
+        formLayout5.addWidget(conferenceTextBox5, 4, 1, 1, 5)
         
-        formLayout2.addWidget(bibtexLabel2, 5, 0)
-        formLayout2.addWidget(extractButton2, 5, 5, alignment=Qt.AlignmentFlag.AlignRight)
-        formLayout2.addWidget(bibtexTextBox2, 6, 0, 1, 6)
+        formLayout5.addWidget(bibtexLabel5, 5, 0)
+        formLayout5.addWidget(extractButton5, 5, 5, alignment=Qt.AlignmentFlag.AlignRight)
+        formLayout5.addWidget(bibtexTextBox5, 6, 0, 1, 6)
 
-        formLayout2.addWidget(idLabel2, 7, 0)
-        formLayout2.addWidget(idTextBox2, 7, 1, 1, 2)
-        formLayout2.addWidget(submitButton2, 7, 0, 4, 6, alignment=Qt.AlignmentFlag.AlignRight)
-        layoutModifyPaper.addLayout(formLayout2)
-        layoutModifyPaper.setContentsMargins(50, 50, 50, 50)
-        tabModifyPaper.setLayout(layoutModifyPaper)
+        formLayout5.addWidget(idLabel5, 7, 0)
+        formLayout5.addWidget(idTextBox5, 7, 1, 1, 2)
+        formLayout5.addWidget(submitButton5, 7, 0, 4, 6, alignment=Qt.AlignmentFlag.AlignRight)
+        layoutModifyMember.addLayout(formLayout5)
+        layoutModifyMember.setContentsMargins(50, 50, 50, 50)
+        tabModifyMember.setLayout(layoutModifyMember)
 
-        # Tab: Delete Paper
-        tabDeletePaper = QWidget()
-        layoutDeletePaper = QVBoxLayout()
+        # Tab: Delete Member
+        tabDeleteMember = QWidget()
+        layoutDeleteMember = QVBoxLayout()
         
-        formLayout3 = QGridLayout()
+        formLayout6 = QGridLayout()
 
-        scroll3 = QScrollArea()
-        scroll3.setWidgetResizable(True)
-        scroll3.setStyleSheet("background-color: #d9d9d9; margin-bottom: 30px;")
+        scroll6 = QScrollArea()
+        scroll6.setWidgetResizable(True)
+        scroll6.setStyleSheet("background-color: #d9d9d9; margin-bottom: 30px;")
 
-        idLabel3 = QLabel("Paper ID")
-        idLabel3.setStyleSheet(labelStyle+"padding-right: 50px;")
-        idTextBox3 = QLineEdit()
-        idTextBox3.setStyleSheet(textBoxStyle)
-        idTextBox3.setMaximumWidth(145)
+        idLabel6 = QLabel("Paper ID")
+        idLabel6.setStyleSheet(labelStyle+"padding-right: 50px;")
+        idTextBox6 = QLineEdit()
+        idTextBox6.setStyleSheet(textBoxStyle)
+        idTextBox6.setMaximumWidth(145)
 
-        deleteButton3 = QPushButton("Delete Paper")
-        deleteButton3.setFixedWidth(300)
-        deleteButton3.clicked.connect(self.deletePaperClicked)
-        deleteButton3.setStyleSheet("font-size: 20px; padding: 10px; border-radius: 15px; background-color: #4972FD; color: white;")
+        deleteButton6 = QPushButton("Delete Paper")
+        deleteButton6.setFixedWidth(300)
+        deleteButton6.clicked.connect(self.deletePaperClicked)
+        deleteButton6.setStyleSheet("font-size: 20px; padding: 10px; border-radius: 15px; background-color: #4972FD; color: white;")
         
         # Adding widgets to Grid Layout
-        formLayout3.addWidget(scroll3, 0, 0, 4, 6)
+        formLayout6.addWidget(scroll6, 0, 0, 4, 6)
 
-        formLayout3.addWidget(idLabel3, 4, 0, 1, 3, alignment=Qt.AlignmentFlag.AlignRight)
-        formLayout3.addWidget(idTextBox3, 4, 3, 1, 3, alignment=Qt.AlignmentFlag.AlignLeft)
-        formLayout3.addWidget(deleteButton3, 5, 0, 1, 6, alignment=Qt.AlignmentFlag.AlignCenter)
-        layoutDeletePaper.addLayout(formLayout3)
-        layoutDeletePaper.setContentsMargins(50, 50, 50, 50)
-        tabDeletePaper.setLayout(layoutDeletePaper)
+        formLayout6.addWidget(idLabel6, 4, 0, 1, 3, alignment=Qt.AlignmentFlag.AlignRight)
+        formLayout6.addWidget(idTextBox6, 4, 3, 1, 3, alignment=Qt.AlignmentFlag.AlignLeft)
+        formLayout6.addWidget(deleteButton6, 5, 0, 1, 6, alignment=Qt.AlignmentFlag.AlignCenter)
+        layoutDeleteMember.addLayout(formLayout6)
+        layoutDeleteMember.setContentsMargins(50, 50, 50, 50)
+        tabDeleteMember.setLayout(layoutDeleteMember)
 
         # Add Tabs to the TabWidget
-        self.tabsTeam.addTab(tabAddPaper, "ADD PAPER")
-        self.tabsTeam.addTab(tabModifyPaper, "MODIFY PAPER")
-        self.tabsTeam.addTab(tabDeletePaper, "DELETE PAPER")
+        self.tabsTeam.addTab(tabAddMember, "ADD MEMBER")
+        self.tabsTeam.addTab(tabModifyMember, "MODIFY MEMBER")
+        self.tabsTeam.addTab(tabDeleteMember, "DELETE MEMBER")
 
         self.tabsTeam.setStyleSheet("""
                                     QTabBar::tab {
@@ -497,51 +511,44 @@ class MainWindow(QMainWindow):
                                     QTabBar::tab:!selected {
                                         font-weight: normal;
                                     }
-                                """)"""
+                                """)
+        self.mainContextLayout = QVBoxLayout()
+        self.mainContextLayout.addWidget(self.tabsTeam)
         
-        mainContext = QWidget()
-        mainContextLayout = QVBoxLayout()
-        mainContextLayout.addWidget(self.tabsPaper)
-        self.mainContext.setLayout(mainContextLayout)
+        self.mainContext = QWidget()
+        self.mainContext.setLayout(self.mainContextLayout)
         self.mainContext.setStyleSheet("background-color: #00022D; padding: 10px; border-radius: 5px;")
         
         horizontalLayout.addWidget(navBar, 5)
-        horizontalLayout.addWidget(mainContext, 15)
+        horizontalLayout.addWidget(self.mainContext, 15)
         
         widget = QWidget()
         widget.setLayout(horizontalLayout)
         self.setCentralWidget(widget)
 
-    def changeMainContext(self, newlayout):
-        #self.mainContextLayout.removeWidget(self.mainContextWidget)  # Remove old widget
-        #self.mainContextWidget.deleteLater()  # Delete old widget to free memory
-
-        #self.mainContextWidget = new_widget  # Set new widget
-        #self.mainContextLayout.addWidget(self.mainContextWidget)  # Add new widget
-        self.mainContext.setLayout(newlayout)
+    def changeMainContextWidget(self, newWidget):
+        pass
 
     def papersButtonClicked(self, status):
-        self.buttonStyleDescriptionClicked = """QPushButton {
-                                                font-size: 25px;
-                                                background-color: rgba(255, 255, 255, 100);
-                                                padding: 20px;
-                                                border-radius: 10px;
-                                                font-weight: bold;
-                                            }
-                                        """
         self.papersButton.setStyleSheet(self.buttonStyleDescriptionClicked)
         self.teamButton.setStyleSheet(self.buttonStyleDescriptionTransparent)
         self.latestNewsButton.setStyleSheet(self.buttonStyleDescriptionTransparent)
+
+        ## To Do: Change the layout or widget of the main context on Nav bar button click
 
     def teamButtonClicked(self, status):
         self.teamButton.setStyleSheet(self.buttonStyleDescriptionClicked)
         self.papersButton.setStyleSheet(self.buttonStyleDescriptionTransparent)
         self.latestNewsButton.setStyleSheet(self.buttonStyleDescriptionTransparent)
 
+        ## To Do: Change the layout or widget of the main context on Nav bar button click
+
     def latestNewsButtonClicked(self, status):
         self.latestNewsButton.setStyleSheet(self.buttonStyleDescriptionClicked)
         self.teamButton.setStyleSheet(self.buttonStyleDescriptionTransparent)
         self.papersButton.setStyleSheet(self.buttonStyleDescriptionTransparent)
+
+        ## To Do: Change the layout or widget of the main context on Nav bar button click
 
     def submitPaperClicked(self):
         print("Submit Paper Clicked")
